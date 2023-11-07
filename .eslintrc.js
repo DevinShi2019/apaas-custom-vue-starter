22d8f61fb454dbb4172a1b8016bc2884d24b9f6b
821917380 <821917380@qq.com>
2023-11-07 13:15:22 +0800

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,8 +5,8 @@ module.exports = {
   },
   extends: ['plugin:vue/recommended', '@vue/standard'],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // 'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    // 'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'space-before-function-paren': 'off',
     // 禁止在计算属性中对属性修改
     'vue/no-side-effects-in-computed-properties': 'off',
